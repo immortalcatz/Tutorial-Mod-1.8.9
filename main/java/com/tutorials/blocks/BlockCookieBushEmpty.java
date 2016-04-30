9301b6651690745f0cd51ae1dfa8c1b50ecd028e
sky01 <skyler@mail.vu>
2016-04-29 23:37:46 -0600

    Tidied up
    Improved texture. Made the cookie bushes children of BlockBush to get
    some handy features. Added .lang file

--- main/java/com/tutorials/blocks/BlockCookieBushEmpty.java
+++ main/java/com/tutorials/blocks/BlockCookieBushEmpty.java
@@ -2,7 +2,7 @@ package com.tutorials.blocks;
 
 import java.util.Random;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockBush;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.AxisAlignedBB;
@@ -12,42 +12,27 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockCookieBushEmpty extends Block
+public class BlockCookieBushEmpty extends BlockBush
 {
 	public BlockCookieBushEmpty()
 	{
-		super(Material.plants);
+		super();
 		this.setTickRandomly(true); // this allows the plant to randomly update
 		this.setStepSound(soundTypeGrass);
+		float f = 0.4F;
+        this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, 0.5F + f, 0.5F + f);
 	}
 
 	@Override
 	public void randomTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
 	{
-		worldIn.setBlockState(pos, TutorialBlocks.cookieBushFull.getDefaultState());
-	}
-
-	public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state)
-	{
-		return null;
-	}
-
-	/**
-	 * Used to determine ambient occlusion and culling when rebuilding chunks for render
-	 */
-	public boolean isOpaqueCube()
-	{
-		return false;
-	}
-
-	public boolean isFullCube()
-	{
-		return false;
-	}
-
-	@SideOnly(Side.CLIENT)
-	public EnumWorldBlockLayer getBlockLayer()
-	{
-		return EnumWorldBlockLayer.CUTOUT;
+		super.randomTick(worldIn, pos, state, rand);
+		// doesn't grow with every update.
+		// 40% of the time it grows
+		final int GROW_CHANCE = 30;
+		if(rand.nextInt(100) < GROW_CHANCE)
+		{
+			worldIn.setBlockState(pos, TutorialBlocks.cookieBushFull.getDefaultState());
+		}
 	}
 }
